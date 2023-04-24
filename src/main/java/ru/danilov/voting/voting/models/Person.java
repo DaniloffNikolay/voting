eb9030a033bab4449ccf864f2723813aefe1f1c9
Nikolai <89162450327@ya.com>
2023-04-24 08:39:23 +0300

    first commit

---
+++ src/main/java/ru/danilov/voting/voting/models/Person.java
@@ -0,0 +1,51 @@
+package ru.danilov.voting.voting.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "role")
+    private String role;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
